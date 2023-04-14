182d87ce5c40d3fb2f687c4a6bf8b84d0277cf9e
sami-alt <goodfellatoo@gmail.com>
2023-04-14 19:39:00 +0300

    wrote backend to work with sqlite3 db.

--- Backend/server.js
+++ Backend/server.js
@@ -1,35 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const fs = require('fs')
 const cors = require('cors')
 
-const sqlite3 = require('sqlite3').verbose()
+const knex = require('knex')({
+    client: 'better-sqlite3',
+    connection: {
+      filename: "./list.db"
+    }
+});
 
-
-const db = new sqlite3.Database('./list.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.error(err.message);
-})
-
-db.run('CREATE TABLE if not exists items (product text, id int primary key, done boolean)')
-
+knex.raw('CREATE TABLE if not exists items (product text, id integer primary key autoincrement, done boolean)')
+    .then(() => console.log('created'))
 
 const getLista = () =>{
-    return new Promise((resolve, reject) => {
-        db.all('select * from items', [], (err, values) => {
-            if (err) {
-                reject(err)
-                return
-            }
-            resolve(values)
-        })
-    })
+    return knex.select('*').from('items')
 }
-
-
-
-
-const saveLista = (newLista) => fs.writeFileSync('./db.json', JSON.stringify({lista: newLista}, null, 2))
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -48,36 +34,40 @@ app.get('/lista', async (req, res) => {
     }
 })
 
-app.get('/lista/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const lista = getLista()
-    const prod = lista.find(prod => prod.id === id)
-    if (prod) {
-        res.json(prod)
-    } else {
-        res.status(404).end()
-    }
-})
-
-app.post('/lista/',(req, res) => {
-    let lista = getLista()
-    const generateID = () =>{
-        let listLenght;
-        if(lista.length < 1){
-            listLenght = 0
-        } else {
-            listLenght = lista.map(n => n.id).sort((a,b)=> a - b).reverse()[0]
-        }
-        return listLenght + 1
-    }
+app.post('/lista/',async (req, res) => {
     const product = {
         product: req.body.product,
-        id : generateID(lista),
         done: false
     }
-    lista = lista.concat(product)
-    saveLista(lista)
+    const createdProducts = await knex('items').insert(product).returning('id')
+    product.id = createdProducts[0].id
+
     res.json(product)
+})
+
+
+
+app.put('/lista/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    let lista = await getLista()
+    const prodToChange = lista.find(prod => prod.id === id)
+    if (!prodToChange) {
+        res.status(404).end()
+        return
+    }
+
+    await knex('items').update(req.body).where('id', id)
+    console.log(req.body)
+
+    Object.assign(prodToChange, req.body)
+    res.json(prodToChange)
+})
+
+app.delete('/lista/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    await knex('items').where('id', id).del()
+    let lista = await getLista()
+    res.json(lista)
 })
 
 app.post('/lista/id',(req,res) => {
@@ -86,31 +76,9 @@ app.post('/lista/id',(req,res) => {
         id:1,
         owner:ownerID,
         content:{...content}
-
     }
-
 })
 
-app.put('/lista/:id', (req, res) => {
-    const id = Number(req.params.id)
-    let lista = getLista()
-    const prodToChange = lista.find(prod => prod.id === id)
-    if (!prodToChange) {
-        res.status(404).end()
-        return
-    }
-    Object.assign(prodToChange, req.body)
-    saveLista(lista)
-    res.json(prodToChange)
-})
-
-app.delete('/lista/:id', (req, res) => {
-    const id = Number(req.params.id)
-    let lista = getLista()
-    lista = lista.filter(prod => prod.id !== id)
-    saveLista(lista)
-    res.status(204).send(null).end()
-})
 
 const port = 3001
 app.listen(port)
