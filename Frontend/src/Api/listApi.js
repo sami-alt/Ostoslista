6d038202bd8ea19d729748d548472dd6b7f25b87
sami-alt <goodfellatoo@gmail.com>
2023-04-26 10:21:52 +0300

    Added endpoints for lists.

--- Frontend/src/Api/listApi.js
+++ Frontend/src/Api/listApi.js
@@ -1,3 +1,34 @@
 import request from "../Utils/request";
 
+export const createList = (name, owner) =>{
+    return request({
+        method:"POST",
+        url:"lista",
+        data:{
+            owner:owner,
+            name:name
+        }
+    })
+}
 
+export const getLists = () =>{
+    return request({
+        method:"GET",
+        url:"listat"
+    })
+
+}
+
+export const getList = (id) => {
+    return request({
+        method:"GET",
+        url:"lista/" + id
+    })
+}
+
+export const deleteList = (id) => {
+    return request({
+        method: "DELETE",
+        url:"lista/" + id
+    })
+}
