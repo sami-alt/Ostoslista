45e73b4bce4009126bc4b6f67d5c5a8e7743f455
sami-alt <goodfellatoo@gmail.com>
2023-04-28 10:35:57 +0300

    delete list by id

---
+++ Frontend/src/Pages/MyLists/deleteList.js
@@ -0,0 +1,18 @@
+import  Button  from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete"
+import {deleteList} from "../../Api/listApi"
+
+const Delete = (props) => {
+    const del = (event) =>{
+        event.preventDefault()
+        deleteList(props.id).then(() => {
+            props.onDelete(props.id)
+        })
+    }
+
+    return(
+        <Button variant="contained" size="small" onClick={del} startIcon={<DeleteIcon/>} ></Button>
+    )
+}
+
+export default Delete
