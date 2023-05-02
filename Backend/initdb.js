5e9b6d33dadcb02882d9e0101825d057db02f569
sami-alt <goodfellatoo@gmail.com>
2023-05-02 19:49:44 +0300

    Added user schema to dd init file.

--- Backend/initdb.js
+++ Backend/initdb.js
@@ -15,12 +15,16 @@ knex.schema.createTable('items', (table)=>{
     table.text('product');
     table.integer('done');
     table.integer('listId').references('id').inTable('lists').notNullable();
-}).then(console.log('table created'))    
+}).then(console.log('items table created'))    
 
 knex.schema.createTable('lists', (table)=>{
     table.integer('id').primary();
     table.text('name');
-    table.integer('owner');
-    
+    table.integer('owner').references('id'),inTable('users').notNullable();
+}).then(console.log('lists table created'))
 
-}).then(console.log('table created'))
+knex.schema.createTable('users', (table)=>{
+    table.increments('id').primary();
+    table.text('username');
+    table.text('password');
+}).then(console.log('user table created'))
