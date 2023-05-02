24e2be744e318856f2e8e0a5fa1c9629654ec750
sami-alt <goodfellatoo@gmail.com>
2023-05-02 19:50:46 +0300

    Changed the server functionality to remove object from the db.

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,8 @@ const knex = require('knex')({
     }
 });
 
-const getList = () =>{
-    return knex.select('*').from('items')
+const getProducts = (id) =>{
+    return knex.select('*').from('items').where('listId',id)
 }
 
 const getLists = () => {
@@ -25,10 +25,9 @@ app.get('/', (req, res) => {
     res.send('Ostoslista')
 })
 
-app.get('/tuotteet', async (req, res) => {
+app.get('/tuotteet/:listId', async (req, res) => {
     try {
-        const values = await getList()
-        console.log(values)
+        const values = await getProducts(req.params.listId)
         res.json(values)
     } catch(err) {
         console.error(err)
@@ -44,13 +43,12 @@ app.post('/tuote/',async (req, res) => {
     }
     const createdProducts = await knex('items').insert(product).returning('id')
     product.id = createdProducts[0].id
-
     res.json(product)
 })
 
 app.put('/tuote/:id', async (req, res) => {
     const id = Number(req.params.id)
-    let lista = await getList()
+    let lista = await getProducts()
     const prodToChange = lista.find(prod => prod.id === id)
     if (!prodToChange) {
         res.status(404).end()
@@ -58,8 +56,6 @@ app.put('/tuote/:id', async (req, res) => {
     }
 
     await knex('items').update(req.body).where('id', id)
-    console.log(req.body)
-
     Object.assign(prodToChange, req.body)
     res.json(prodToChange)
 })
@@ -67,14 +63,14 @@ app.put('/tuote/:id', async (req, res) => {
 app.delete('/tuote/:id', async (req, res) => {
     const id = Number(req.params.id)
     await knex('items').where('id', id).del()
-    let lista = await getLists()
-    res.json(lista)
+    res.json({id})
 })
 
 app.get('/listat', async(req, res) => {
     try {
     const lists = await getLists()
     res.json(lists)
+    //console.log(lists)
     }catch(err) {
         console.log(err)
         res.status(500).json({error: String(err)})
@@ -86,13 +82,20 @@ app.post('/lista', async (req,res) => {
     const list = {
         name: req.body.name, 
         owner:req.body.owner
-    }
+        }
         const createdList = await knex('lists').insert(list).returning('id')
         list.id = createdList[0].id
-
+        //console.log(list)
         res.json(list)
 })
 
+app.delete('/lista/:id', async(req ,res) => {
+    const id = Number(req.params.id)
+    await knex('items').where('listId', id).del()
+    await knex('lists').where('id', id).del()
+    
+    res.json({id})
+})
 
 const port = 3001
 app.listen(port)
